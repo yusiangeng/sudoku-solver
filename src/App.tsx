6ae572a4c2fbf0878bd1e19ff19245cfebfb9fa1
yusiangeng <engyusiang@icloud.com>
2023-01-05 12:36:51 +0800

    Add subgrid check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-const ROWS = 3;
+const ROWS = 9;
 
 function parseText(text: string): number[][] {
   const parsed: number[][] = [];
@@ -43,36 +43,53 @@ function isValidInput(input: number[][]): boolean {
   }
 
   // check each row for duplicates
-  for (let row = 0; row < input.length; row++) {
-    const counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    for (let col = 0; col < input.length; col++) {
+  for (let row = 0; row < ROWS; row++) {
+    const foundNums = new Set();
+    for (let col = 0; col < ROWS; col++) {
       const num = input[row][col];
-      counts[num]++;
-    }
-    for (let i = 1; i <= 9; i++) {
-      if (counts[i] > 1) {
-        console.log("failed row check");
-        return false;
+      if (num >= 1 && num <= 9) {
+        if (foundNums.has(num)) {
+          console.log("failed row check");
+          return false;
+        }
+        foundNums.add(num);
       }
     }
   }
 
   // check each column for duplicates
-  for (let col = 0; col < input.length; col++) {
-    const counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    for (let row = 0; row < input.length; row++) {
+  for (let col = 0; col < ROWS; col++) {
+    const foundNums = new Set();
+    for (let row = 0; row < ROWS; row++) {
       const num = input[row][col];
-      counts[num]++;
-    }
-    for (let i = 1; i <= 9; i++) {
-      if (counts[i] > 1) {
-        console.log("failed col check");
-        return false;
+      if (num >= 1 && num <= 9) {
+        if (foundNums.has(num)) {
+          console.log("failed col check");
+          return false;
+        }
+        foundNums.add(num);
       }
     }
   }
 
   // check each subgrid for duplicates
+  for (let subgridRow = 0; subgridRow < 3; subgridRow++) {
+    for (let subgridCol = 0; subgridCol < 3; subgridCol++) {
+      const foundNums = new Set();
+      for (let row = subgridRow * 3; row < (subgridRow + 1) * 3; row++) {
+        for (let col = subgridCol * 3; col < (subgridCol + 1) * 3; col++) {
+          const num = input[row][col];
+          if (num >= 1 && num <= 9) {
+            if (foundNums.has(num)) {
+              console.log("failed subgrid check");
+              return false;
+            }
+            foundNums.add(num);
+          }
+        }
+      }
+    }
+  }
 
   return true;
 }
@@ -99,7 +116,12 @@ function App() {
 
   return (
     <>
+      <p>Each row on new line</p>
+      <p>For each row, separate values by comma</p>
+      <p>Use 0 or space or nothing to represent empty cell</p>
       <textarea
+        rows={10}
+        cols={20}
         onChange={(e) => {
           test(e.target.value);
         }}
